76e133cd54dfd6702ac8d0c6b1a434a6f651f3fa
eslamfront55 <eslamfront55@gmail.com>
2024-03-31 19:39:23 +0200

    "Added form controls and validation to edit client component; updated form field styles; added new variable for secondary border color"
    This commit message describes the changes made in the code diff, including the addition of form controls and validation to the edit client component, updates to form field styles, and the addition of a new variable for the secondary border color. It is concise and accurately describes the changes made in the code.

--- src/app/components/dashboard/clients/edit-client/edit-client.component.ts
+++ src/app/components/dashboard/clients/edit-client/edit-client.component.ts
@@ -1,8 +1,10 @@
+import { PublicService } from './../../../../services/generic/public.service';
 import { UploadMultiFilesComponent } from './../../../../shared/components/upload-multi-files/upload-multi-files.component';
 import { FileUploadComponent } from './../../../../shared/components/file-upload/file-upload.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   standalone: true,
@@ -15,6 +17,41 @@ export class EditClientComponent {
   isEditFile: boolean = false;
   imageSrc: string = '';
   // imageSrc: string = 'assets/images/home/sidebar-bg.webp';
+
+  modalForm = this.fb?.group(
+    {
+      fullName: ['', {
+        validators: [
+          Validators.required,
+          Validators?.minLength(3)], updateOn: "blur"
+      }],
+      id: ['', {
+        validators: [
+          Validators.required], updateOn: "blur"
+      }],
+      phoneNumber: ['', {
+        validators: [
+          Validators.required], updateOn: "blur"
+      }],
+
+      birthDate: [null, {
+        validators: [
+          Validators.required]
+      }],
+      country: [null, {
+        validators: [
+          Validators.required]
+      }],
+    }
+  );
+
+  get formControls(): any {
+    return this.modalForm?.controls;
+  }
+  constructor(
+    public publicService: PublicService,
+    private fb: FormBuilder,
+  ) { }
   uploadFile(event: any): void {
     console.log(event);
   }
