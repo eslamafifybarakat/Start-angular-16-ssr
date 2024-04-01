858c774951da7de5a38726f0f87597591c1dcba2
eslamfront55 <eslamfront55@gmail.com>
2024-04-01 12:40:32 +0200

    Add license, certificate, medical, and file details to client edit page
    The changes include:
    
    * Adding new fields for license, certificate, medical, and file details in the client edit page HTML.
    * Adding corresponding form controls and validation in the client edit page TypeScript.
    * Adding translations for the new fields in Arabic and English JSON files.
    
    No changes were made to the existing functionality of the client edit page.

--- src/app/components/dashboard/clients/edit-client/edit-client.component.ts
+++ src/app/components/dashboard/clients/edit-client/edit-client.component.ts
@@ -17,15 +17,43 @@ import { CalendarModule } from 'primeng/calendar';
 export class EditClientComponent {
   isRegistrationNumberReadOnly: boolean = true;
   isRecordDateReadOnly: boolean = true;
-  isEditRegistrationFile: boolean = false;
+  isLicenseNumberReadOnly: boolean = true;
+  isLicenseDateReadOnly: boolean = true;
+  isCertificateNumberReadOnly: boolean = true;
+  isCertificateDateReadOnly: boolean = true;
+  isMedicalInsuranceNumberReadOnly: boolean = true;
+  isMedicalInsuranceDateReadOnly: boolean = true;
+  isBusinessLicenseReadOnly: boolean = true;
+  isBusinessLicenseNumberReadOnly: boolean = true;
 
   details: any = {
-    registrationNumber: '332111111',
+    registrationNumber: '2135836527289',
     recordDate: new Date(),
     registrationFile: 'assets/images/home/sidebar-bg.webp',
+
+    licenseDate: new Date(),
+    licenseNumber: '135836527289',
+    licenseFile: 'assets/images/home/sidebar-bg.webp',
+
+    certificateDate: new Date(),
+    certificateNumber: '135836527289',
+    certificateFile: 'assets/images/home/sidebar-bg.webp',
+
+    medicalInsuranceDate: new Date(),
+    medicalInsuranceNumber: '135836527289',
+
+    businessLicense: "business License 1",
+    businessLicenseNumber: "135836527289"
   };
+
+  isEditRegistrationFile: boolean = false;
   registrationFile: string = '';
-  // imageSrc: string = 'assets/images/home/sidebar-bg.webp';
+
+  isEditLicenseFile: boolean = false;
+  licenseFile: string = '';
+
+  isEditCertificateFile: boolean = false;
+  certificateFile: string = '';
 
   modalForm = this.fb?.group(
     {
@@ -37,18 +65,37 @@ export class EditClientComponent {
         validators: [
           Validators.required]
       }],
-      id: ['', {
+      licenseNumber: ['', {
         validators: [
           Validators.required], updateOn: "blur"
       }],
-      phoneNumber: ['', {
-        validators: [
-          Validators.required], updateOn: "blur"
-      }],
-
-      country: [null, {
+      licenseDate: [null, {
         validators: [
           Validators.required]
+      }],
+      certificateNumber: ['', {
+        validators: [
+          Validators.required], updateOn: "blur"
+      }],
+      certificateDate: [null, {
+        validators: [
+          Validators.required]
+      }],
+      medicalInsuranceNumber: ['', {
+        validators: [
+          Validators.required], updateOn: "blur"
+      }],
+      medicalInsuranceDate: [null, {
+        validators: [
+          Validators.required]
+      }],
+      businessLicense: ['', {
+        validators: [
+          Validators.required], updateOn: "blur"
+      }],
+      businessLicenseNumber: ['', {
+        validators: [
+          Validators.required], updateOn: "blur"
       }],
     }
   );
@@ -70,9 +117,21 @@ export class EditClientComponent {
     this.modalForm?.patchValue({
       registrationNumber: this.details?.registrationNumber,
       recordDate: this.details?.recordDate,
+      licenseNumber: this.details?.registrationNumber,
+      licenseDate: this.details?.recordDate,
+      certificateNumber: this.details?.certificateNumber,
+      certificateDate: this.details?.certificateDate,
+      medicalInsuranceNumber: this.details?.medicalInsuranceNumber,
+      medicalInsuranceDate: this.details?.medicalInsuranceDate,
+      businessLicenseNumber: this.details?.businessLicenseNumber,
+      businessLicense: this.details?.businessLicense,
     })
     this.isEditRegistrationFile = true;
     this.registrationFile = this.details?.registrationFile;
+    this.isEditLicenseFile = true;
+    this.licenseFile = this.details?.licenseFile;
+    this.isEditCertificateFile = true;
+    this.certificateFile = this.details?.licenseFile;
   }
   editInput(name: string): void {
     if (name == 'registrationNumber') {
@@ -80,6 +139,30 @@ export class EditClientComponent {
     }
     if (name == 'recordDate') {
       this.isRecordDateReadOnly = false;
+    }
+    if (name == 'licenseNumber') {
+      this.isLicenseNumberReadOnly = false;
+    }
+    if (name == 'licenseDate') {
+      this.isLicenseDateReadOnly = false;
+    }
+    if (name == 'certificateNumber') {
+      this.isCertificateNumberReadOnly = false;
+    }
+    if (name == 'certificateDate') {
+      this.isCertificateDateReadOnly = false;
+    }
+    if (name == 'medicalInsuranceNumber') {
+      this.isMedicalInsuranceNumberReadOnly = false;
+    }
+    if (name == 'medicalInsuranceDate') {
+      this.isMedicalInsuranceDateReadOnly = false;
+    }
+    if (name == 'businessLicense') {
+      this.isBusinessLicenseReadOnly = false;
+    }
+    if (name == 'businessLicenseNumber') {
+      this.isBusinessLicenseNumberReadOnly = false;
     }
   }
 
